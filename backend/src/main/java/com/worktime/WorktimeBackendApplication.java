56b5a6585d2e0da149dc77ba5ae8c9c71076a1c5
abugrakskn <abugrakskn52@gmail.com>
2026-07-24 09:56:52 +0300

    chore: initialize Spring Boot backend project

---
+++ backend/src/main/java/com/worktime/WorktimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.worktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktimeBackendApplication.class, args);
+	}
+
+}
