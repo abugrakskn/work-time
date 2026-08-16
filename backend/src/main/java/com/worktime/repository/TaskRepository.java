ee98db301a84dfd7095898b2eaaa446b0d3dd9c3
abugrakskn <abugrakskn52@gmail.com>
2026-08-16 21:30:51 +0300

    feat: add overdue task query and dashboard display

--- backend/src/main/java/com/worktime/repository/TaskRepository.java
+++ backend/src/main/java/com/worktime/repository/TaskRepository.java
@@ -2,10 +2,12 @@ package com.worktime.repository;
 
 import com.worktime.entity.Project;
 import com.worktime.entity.Task;
+import com.worktime.entity.TaskStatus;
 import com.worktime.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -19,5 +21,17 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     boolean existsByProjectAndDueDateAfter(
             Project project,
             LocalDate endDate
+    );
+
+    List<Task> findByDueDateBeforeAndStatusInOrderByDueDateAsc(
+            LocalDate dueDate,
+            Collection<TaskStatus> statuses
+    );
+
+    List<Task>
+    findByAssignedUserAndDueDateBeforeAndStatusInOrderByDueDateAsc(
+            User assignedUser,
+            LocalDate dueDate,
+            Collection<TaskStatus> statuses
     );
 }
